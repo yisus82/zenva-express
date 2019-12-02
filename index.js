699b700d4380e3e2959ecf776ca8f7f69c15e583
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-12-02 09:20:48 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res, next) => {
+  res.send('My First Express App!');
+});
+
+app.get('/json', (req, res, next) => {
+  const data = { name: 'David', location: 'Sydney' };
+  res.json(data);
+});
+
+app.get('/html', (req, res, next) => {
+  const html =
+    '<html><h1 style="color:red">This is an HTML response</h1></html>';
+  res.send(html);
+});
+
+app.listen(5000);
+console.log('Server running on http://localhost:5000');
