2e53a47f07f655a7f45ba5279744697194882478
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-12-02 11:32:02 +0100

    Static assets

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use('/', routes);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(5000);
 console.log('Server running on http://localhost:5000');
